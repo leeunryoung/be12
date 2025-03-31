196b83364dbab00c6f955e7f886322bc4251b6dc
un_yong <cristeaa@naver.com>
2025-03-31 16:00:52 +0900

    hollw

---
+++ src/Hello_git.java
@@ -0,0 +1,9 @@
+public class Hello_git {
+    public static void main(String[] args) {
+        System.out.println("git file 보세요");
+        System.out.println("git file 보세요");
+        System.out.println("git file 보세요");
+        System.out.println("git file 보세요");
+    }
+
+    }
